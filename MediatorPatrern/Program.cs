828bab3448e53b96586202d6cc4f6d8d8e5737bb
Tony_Tsai <tonytsai27@gmail.com>
2022-01-06 16:14:00 +0800

    協調者模式

---
+++ MediatorPatrern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPatrern
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	public interface ICartService
+	{
+
+	}
+	public interface IOrderService
+	{ 
+	}
+	public interface IStockService
+	{
+
+	}
+}
