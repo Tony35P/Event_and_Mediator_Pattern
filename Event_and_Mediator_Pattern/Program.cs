d97bc0578f89a79dba69fc9842b332b7ba994d01
Tony_Tsai <tonytsai27@gmail.com>
2022-01-06 15:42:25 +0800

    爸爸有的繼承也會有

--- Event_and_Mediator_Pattern/Program.cs
+++ Event_and_Mediator_Pattern/Program.cs
@@ -10,19 +10,23 @@ namespace Event_and_Mediator_Pattern
 	{
 		static void Main(string[] args)
 		{
-			Member member = new Member();
+			//Member member = new Member();
 
-			member.DataChange += Member_DataChange; //設定要先講
-			member.Name = "Allen"; // 這裡要觸發
-			string name = member.Name;
+			//member.DataChange += Member_DataChange; //設定要先講
+			//member.Name = "Allen"; // 這裡要觸發
+			//string name = member.Name;
 
-			Member member2 = new Member();
-			member2.DataChange += Member_DataChange;
-			member2.Name = "Tony";
+			//Member member2 = new Member();
+			//member2.DataChange += Member_DataChange;
+			//member2.Name = "Tony";
 
+
+			VIP vip = new VIP();
+			vip.DataChange += Member_DataChange;
+			vip.Name = "Tony";
 		}
 
-		
+
 
 		private static void Member_DataChange(Member sender, EventArgs arg)
 		{
@@ -51,5 +55,10 @@ namespace Event_and_Mediator_Pattern
 				 
 			}
 		}
+	}
+
+	class VIP:Member
+	{
+
 	}
 }
