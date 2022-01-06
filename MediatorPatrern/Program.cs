9e6d4af9497deb1f0675550c838864cb489d4270
Tony_Tsai <tonytsai27@gmail.com>
2022-01-06 17:06:24 +0800

    Main裡, 只要CheckOut() 逼逼叫就好了, 這裡寫的比較簡單Update()和EmptyCart()傳的參數不只這些

--- MediatorPatrern/Program.cs
+++ MediatorPatrern/Program.cs
@@ -10,6 +10,14 @@ namespace MediatorPatrern
 	{
 		static void Main(string[] args)
 		{
+			ICartService cart = new CartService();
+			IOrderService order = new OrderService();
+			IStockService stock = new StockService();
+			
+			var mediator = new CartMediator(cart,order,stock);
+
+			int productId = 99;
+			cart.CheckOut(productId); //逼逼叫
 		}
 	}
 
@@ -24,18 +32,69 @@ namespace MediatorPatrern
 			_cart = cart;
 			_order = order;
 			_stock = stock;
+
+			_cart.RequestCheckOut += _cart_RequestCheckOut;
+		}
+
+		private void _cart_RequestCheckOut(ICartService sender, int productId)
+		{
+			_order.PlaceOrder(productId); //下訂單
+			_stock.Update(productId,1); // 減少庫存
+			_cart.EmptyCart(); // 清空購物車
+		}
+	}
+	public class CartService : ICartService
+	{
+		public event CheckOutEventHandler RequestCheckOut;
+
+		public void CheckOut(int productId)
+		{
+			OnRequestCheckout(productId);
+		}
+
+		public void EmptyCart()
+		{
+			Console.WriteLine("購物車已清空");
+		}
+
+		protected virtual void OnRequestCheckout(int prodctId)
+		{
+			if(RequestCheckOut != null)
+			{
+				RequestCheckOut(this, prodctId);
+			}
+		}
+
+	}
+	public class OrderService : IOrderService
+	{
+		public void PlaceOrder(int productId)
+		{
+			Console.WriteLine("OrderService建立一筆新訂單");
 		}
 	}
 
+	public class StockService : IStockService
+	{
+		public void Update(int productId, int qty)
+		{
+			Console.WriteLine($"商品編號{productId} 少了{qty}");
+		}
+	}
+
+	public delegate void CheckOutEventHandler(ICartService sender, int productId);
 	public interface ICartService
 	{
-
+		event CheckOutEventHandler RequestCheckOut;
+		void CheckOut(int productId);
+		void EmptyCart();
 	}
 	public interface IOrderService
-	{ 
+	{
+		void PlaceOrder(int productId);
 	}
 	public interface IStockService
 	{
-
+		void Update(int productId, int qty);
 	}
 }
