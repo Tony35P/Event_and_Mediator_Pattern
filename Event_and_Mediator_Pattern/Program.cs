0868bb2bbbe756d31bbf28c4a3a1929cb7b7a01b
Tony_Tsai <tonytsai27@gmail.com>
2022-01-06 15:06:15 +0800

    先寫一個delegate, 在class寫event, 並在要觸發的地方呼叫event

--- Event_and_Mediator_Pattern/Program.cs
+++ Event_and_Mediator_Pattern/Program.cs
@@ -14,17 +14,27 @@ namespace Event_and_Mediator_Pattern
 			member.Name = "Allen";
 
 			string name = member.Name;
+
+
 		}
 	}
 
+	delegate void DataChangeHandler();
+
 	class Member
 	{
+		public event DataChangeHandler DataChange;
+		
 		private string _Name;
 
 		public string Name
 		{
 			get { return _Name; }
-			set { _Name = value; }
+			set 
+			{
+				DataChange();
+				_Name = value; 
+			}
 		}
 	}
 }
